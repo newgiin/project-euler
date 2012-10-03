33a9698a700e16ea6e637fbf025c577668b1a0af
Andrew Nguyen <atnguyen4@gmail.com>
2012-10-03 00:37:01 -0400

    e54: Basic hand comparison implemented.

--- euler.py
+++ euler.py
@@ -916,27 +916,139 @@ def e_59():
             return
     print "FAILED"
 
+""" Container for find_repeat_vals result """
+class _RepeatValsResult:
+    is_4_kind = is_3_kind = is_2_pair = is_pair = False
+    val = 0 # value of a {3,4}-of-a-kind
+    pair_1_val = 0
+    pair_2_val = 0
+    remainder = []
+
+""" Determines if a hand contains pairs, or a {3,4}-of-a-kind """    
+def find_repeat_vals(hand):
+    vals = {} # Map: val -> # of cards with value of 'val' in hand
+    result = _RepeatValsResult()
+    for card in hand:
+        if (card.val in vals):
+            vals[card.val] += 1
+        else:
+            vals[card.val] = 1
+            
+    for val in vals:
+        if (vals[val] == 4):
+            result.is_4_kind = True
+            result.val = val
+        elif (vals[val] == 3):
+            result.is_3_kind = True
+            result.val = val
+        elif (vals[val] == 2):
+            if (result.pair_1_val == 0):
+                result.is_pair = True
+                result.pair_1_val = val
+            else:
+                result.is_two_pair = True
+                result.pair_2_val = val
+        else:
+            result.remainder.append(val)
+    return result
+
+""" 
+Container for find_str8() result. Also stores high-card since find_str8() sorts the 
+hand allowing easy access to highcard.
+"""      
+class _FindStr8Result:
+    high_card = 0
+    has_str8 = False
+            
+def find_str8(hand):
+    result = _FindStr8Result()
+    srted = [card.val for card in hand]
+    srted.sort()
+    result.high_card = srted[-1]
+    for i in range(0, len(srted)-1):
+        if (srted[i] + 1 !=  srted[i+1]):
+            return result
+    result.has_str8 = True
+    return result
 
 def has_flush(hand):
-    pass
+    suit = hand[0].suit
+    for i in range(1, len(hand)):
+        if (hand[i].suit != suit):
+            return False
+    return True  
 
-def has_str8_flush(hand):
-    if (is_str8(hand) and has_flush(hand)):
-        return True
-    return False        
-
-class Suit:
-    HEART, DIAMOND, SPADE, CLUB = range(4)
-    
-class Card:
+class _Card:
     def __init__(self, val, suit):
         self.val = val
         self.suit = suit
         
-def e_54():
-    f = open(INPUT_DIR + "poker.txt", "r")
+    def __init__(self, s):
+        try:
+            self.val = int(s[0])
+        except ValueError:
+            if (s[0] == "J"):
+                self.val = 11
+            elif (s[0] == "Q"):
+                self.val = 12
+            elif (s[0] == "K"):
+                self.val = 13
+            else:
+                self.val = 14
+        self.suit = s[1]
+        
+class _HandRank:
+    HIGH_CARD, PAIR, TWO_PAIR, THREE_KIND, STR8, FLUSH\
+    , FULL_HOUSE, FOUR_KIND, STR8_FLUSH = range(9)
     
-
+def rank_hand(hand):
+    rank = _HandRank.HIGH_CARD
+    rep_result = find_repeat_vals(hand)
+    str8_result = find_str8(hand)
+    is_flush = has_flush(hand)   
+    
+    if (rep_result.is_2_pair):
+        rank = _HandRank.TWO_PAIR
+    elif (rep_result.is_pair):
+        rank = _HandRank.PAIR
+        if (rep_result.is_3_kind):
+            rank = _HandRank.FULL_HOUSE
+    elif (rep_result.is_3_kind):
+        rank = _HandRank.THREE_KIND
+    elif (rep_result.is_4_kind):
+        rank = _HandRank.FOUR_KIND
+    elif (str8_result.has_str8):
+        rank = _HandRank.STR8
+        if (is_flush):
+            rank = _HandRank.STR8_FLUSH
+    elif (is_flush):
+        rank = _HandRank.FLUSH
+        
+    return rank    
+        
+def is_winning_hand(hand_1, hand_2):
+    if (rank_hand(hand_1) == rank_hand(hand_2)):
+        return True
+    return False
+                   
+def e_54():
+    result = 0
+    f = open(INPUT_DIR + "poker.txt", "r")
+    for line in f:
+        cards = line.split()
+        p1_hand = []
+        p2_hand = []
+        # populate hands
+        for i in range(0, 5):
+            p1_hand.append(_Card(cards[i]))
+        for i in range(5, 10):
+            p2_hand.append(_Card(cards[i]))           
+        # determine winner
+        if (is_winning_hand(p1_hand, p2_hand)):
+            print line
+            result += 1
+    print result
+    
 def e_57():
     result = 0
     acc = fractions.Fraction(1, 2)
