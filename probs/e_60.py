1734293998f07d4a38232afac08a97e7a3b0483c
Andrew Nguyen <atnguyen4@gmail.com>
2013-06-19 16:47:22 -0400

    finished #60

--- probs/e_60.py
+++ probs/e_60.py
@@ -1,49 +1,46 @@
 #!/usr/bin/python
 import e_util
-
+    
+def find_longest_seq(n, ps, g, pres):
+    longest = []
+    for nghbr in g[n]:
+        go = True
+        for pre in pres:
+            if nghbr not in g[pre]:
+                go = False
+                break
+        if go:
+            seq = find_longest_seq(nghbr, ps, g, pres + [n])
+            if (len(seq) > len(longest) or
+                    len(seq) == len(longest) and
+                    sum(seq) < sum(longest)):
+                longest = seq
+            
+    return [ps[n]] + longest
+    
 def main():
     n = 5
     ps = e_util.get_primes(10000)[1:] # don't need '2'
     g = {}
     for i in xrange(len(ps)):
         p_i = ps[i]
-        g[i] = []
+        g[i] = set()
         for j in xrange(i+1, len(ps)):
             p_j = ps[j]
-            if (e_util.is_prime(int(str(p_i) + str(p_j))) and
-                    e_util.is_prime(int(str(p_j) + str(p_i)))):
-                g[i].append(j)
+            if (e_util.is_prime(int('%d%d'%(p_i,p_j))) and
+                    e_util.is_prime(int('%d%d'%(p_j,p_i)))):
+                g[i].add(j)
                 
-                
-    lowest = float('inf')
-    lengths = [(1, None)]*len(ps) # i -> (length, neighbor))
-    for i in reversed(xrange(len(ps))):
-        max_len = 0
-        max_nghbr = None
-        for nghbr in g[i]:
-            if lengths[nghbr][0] > max_len:
-                max_len = lengths[nghbr][0]
-                max_nghbr = nghbr
-        lengths[i] = (1 + max_len, max_nghbr)
-    print 'done calculating lengths'
-    targets = []
-    for i in xrange(len(lengths)):
-        if lengths[i][0] >= 5:
-            j = lengths[i][1]
-            seq = [ps[i]]
-            while j:
-                seq.append(ps[j])
-                j = lengths[j][1]
-        targets.append(seq)
+    longest = []
     min_sum = float('inf')
-    for seq in targets:
-        curr_sum = sum(seq)
-        if curr_sum < min_sum:
-            min_sum = curr_sum
-            print seq
-    print curr_sum
-            
+    for i in xrange(len(ps) - (n - 1)):
+        seq = find_longest_seq(i, ps, g, [])
+        if (len(seq) > len(longest) or
+                len(seq) == len(longest) and
+                sum(seq) < sum(longest)):
+            longest = seq
+            min_sum = sum(seq)   
+    print min_sum
     
-      
 if __name__ == '__main__':
     main()
