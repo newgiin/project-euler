5b82646d84f135155bd1c468608cb4a2e97bfbbc
Andrew Nguyen <atnguyen4@gmail.com>
2013-11-20 23:24:47 -0500

    Use DP for #31

--- probs/e_31.py
+++ probs/e_31.py
@@ -1,21 +1,14 @@
+def change_combos(n, denoms):
+    a = [0] * (n+1)
+    a[0] = 1
+    for d in denoms:
+        for i in xrange(d, n+1):
+            a[i] += a[i-d]    
+    return a[n]
+
 def main():
     denoms = [1, 2, 5, 10, 20, 50, 100, 200]
-
-    def combo_recurr(n, max_denom):
-        if max_denom == 1:
-            return 1
-        remainder = n - max_denom
-        if remainder == 0:
-            return 1 + combo_recurr(n, denoms[denoms.index(max_denom) - 1])
-        if remainder < 0:
-            return combo_recurr(n, denoms[denoms.index(max_denom) - 1])
-        return combo_recurr(remainder, max_denom) + combo_recurr(n, denoms[denoms.index(max_denom) - 1])
-    
-    def combos(n):
-        return combo_recurr(n, 200)
-    
-    
-    print combos(200)
+    print change_combos(200, denoms)
 
 if __name__ == '__main__':
     main()
