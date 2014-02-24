37db8e25e6fbe7142525aada30a36d6d63e2e4c7
Andrew Nguyen <atnguyen4@gmail.com>
2014-02-24 15:25:43 -0500

    Refactor 81.

--- probs/e_81.py
+++ probs/e_81.py
@@ -1,5 +1,5 @@
 def main():
-    f = open(INPUT_DIR + "matrix.txt", "r")
+    f = open("../in_files/matrix.txt", "r")
     arr = [map(int, line.split(",")) for line in f]
     
     num_rows = len(arr)
@@ -10,19 +10,9 @@ def main():
     for row in xrange(1, num_rows):
         arr[row][0] += arr[row - 1][0]
         
-    curr = 1
-    while curr < num_rows:
-        for col in xrange(curr, num_cols):
-            if arr[curr][col - 1] < arr[curr - 1][col]:
-                arr[curr][col] += arr[curr][col - 1]
-            else:
-                arr[curr][col] += arr[curr - 1][col]
-        for row in xrange(curr + 1, num_rows):
-            if arr[row][curr - 1] < arr[row - 1][curr]:
-                arr[row][curr] += arr[row][curr - 1]
-            else:
-                arr[row][curr] += arr[row - 1][curr]
-        curr += 1
+    for row in xrange(1, num_rows):
+        for col in xrange(1, num_cols):
+            arr[row][col] += min(arr[row-1][col], arr[row][col-1])
     print arr[num_rows - 1][num_cols - 1]
 
 if __name__ == '__main__':
